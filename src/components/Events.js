0dff1e424e6f8b5665f8fcb0a091b93820e09a40
Arturo <alopez96@ucsc.edu>
2019-10-05 12:40:26 -0700

    pagination working, bug with intial load

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,14 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Event from './Event';
+
 
 function Events({ data, paginationData }){
 
+    const [pageList, setPageList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [listLength , setListLength] = useState(0);
+
+    var numberOfPages = 0;
+    const numberPerPage = 4;
+
     useEffect(() => {
-        console.log('data', data)
-        listLength = paginationData.object_count;
-        list = data;
+        console.log('data', data);
+        setListLength(paginationData.object_count);
         load();
-      });
+      }, []);
+
+    const getNumberOfPages = () => {
+        return Math.ceil(listLength / numberPerPage);
+    }
+
+    const firstPage = () => {
+        setCurrentPage(1);
+        loadList();
+    }
+
+    const nextPage = (current) => {
+        setCurrentPage(current + 1);
+        loadList();
+    }
+
+    const prevPage = (current) => {
+        setCurrentPage(current - 1);
+        loadList();
+    }
+
+    const lastPage = () => {
+        setCurrentPage(getNumberOfPages());
+        loadList();
+    }
+
+    const load = () => {
+        numberOfPages = getNumberOfPages();
+        console.log('num of pages', numberOfPages);
+        loadList();
+    }
+
+    const loadList = () => {
+        let begin = ((currentPage - 1) * numberPerPage);
+        let end = begin + numberPerPage;
+
+        setPageList(data.slice(begin, end));
+        console.log('pagelist', pageList);
+        console.log('pagelist length', pageList.length);
+    }
 
     const eventItems = pageList.map((event, key) => {
         return(
@@ -25,10 +71,18 @@ function Events({ data, paginationData }){
                 {eventItems}
             </div>
             :null}
-            <button onClick={()=>firstPage()}>First</button>
-            <button onClick={()=>nextPage()}>Next</button>
-            <button onClick={()=>prevPage()}>Previous</button>
-            <button onClick={()=>lastPage()}>Last</button>
+            <button
+                className='f6 link dim ph3 pv2 mb2 dib white bg-black'
+                onClick={()=>firstPage()}>First</button>
+            <button 
+                className='f6 link dim ph3 pv2 mb2 dib white bg-black'
+                onClick={()=>nextPage(currentPage)}>Next</button>
+            <button 
+                className='f6 link dim ph3 pv2 mb2 dib white bg-black'
+                onClick={()=>prevPage(currentPage)}>Previous</button>
+            <button
+                className='f6 link dim ph3 pv2 mb2 dib white bg-black'
+                onClick={()=>lastPage()}>Last</button>
         </div>
     )
 }
@@ -36,51 +90,5 @@ function Events({ data, paginationData }){
 export default Events;
 
 
-
-//Pagination data and calculations
-//source: https://www.thatsoftwaredude.com/content/6125/how-to-paginate-through-a-collection-in-javascript
-var listLength = 0;
-var list = [];              //store data in list
-var pageList = [];          //data that will show per screen
-var currentPage = 1;
-var numberPerPage = 20;     //number of items per page
-var numberOfPages = 1;      // calculates the total number of pages
-
-function load(){
-    numberOfPages = getNumberOfPages();
-    console.log('num of pages', numberOfPages);
-    loadList();
-}
-
-function getNumberOfPages() {
-    return Math.ceil(listLength / numberPerPage);
-}
-
-function nextPage() {
-    currentPage += 1;
-    loadList();
-}
-
-function prevPage() {
-    currentPage -= 1;
-    loadList();
-}
-
-function firstPage() {
-    currentPage = 1;
-    loadList();
-}
-
-function lastPage() {
-    currentPage = numberOfPages;
-    loadList();
-}
-
-function loadList() {
-    var begin = ((currentPage - 1) * numberPerPage);
-    var end = begin + numberPerPage;
-
-    pageList = list.slice(begin, end);
-    console.log('pagelist', pageList)
-    console.log('pagelist length', pageList.length)
-}
+//Pagination data and calculations source: 
+//https://www.thatsoftwaredude.com/content/6125/how-to-paginate-through-a-collection-in-javascript
